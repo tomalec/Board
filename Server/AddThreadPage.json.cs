8881825fca43737d4afeb666234d28d291a2d427
Tomek Wytrebowicz <tomalecpub@gmail.com>
2014-08-04 12:41:43 +0200

    Add thread-author relation

--- Server/AddThreadPage.json.cs
+++ Server/AddThreadPage.json.cs
@@ -1,11 +1,35 @@
 using Starcounter;
+using Concepts.Ring1;
+using Concepts.Ring2;
 
 [AddThreadPage_json]
 partial class AddThreadPage : Page
 {
     void Handle(Input.Save input)
     {
+        //get somebody out of email
+            Somebody somebody = null;
+
+
+            EMailAddress emailRelation = Db.SQL<EMailAddress>("SELECT o FROM EMailAddress o Where o.Name=?", this.Email).First;
+            if (emailRelation != null && emailRelation.ToWhat is Somebody)
+            {
+                somebody = (Somebody)emailRelation.ToWhat;
+            }
+            else
+            {
+                somebody = new Somebody();
+                somebody.Name = this.Email;
+
+                EMailAddress emailRel = new EMailAddress();
+                emailRel.SetToWhat(somebody);
+                emailRel.EMail = this.Email;
+            }
+            Board.AuthorRelation.CreateAuthorRelation(somebody, (Board.Thread)this.Data);
+
+
         Transaction.Commit();
+
         RedirectUrl = "/launcher/workspace/board/threads/add";
         Data = new Board.Thread();
     }
